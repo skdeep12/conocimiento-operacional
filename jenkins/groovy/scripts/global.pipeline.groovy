552e86211077d8dc570833416711d33a622c9adc
Subhash Kumar Samota <subhashsamota12@gmail.com>
2020-05-28 14:22:49 +0530

    Create global.pipeline.groovy

---
+++ jenkins/groovy/scripts/global.pipeline.groovy
@@ -0,0 +1,45 @@
+import hudson.scm.SCM
+import jenkins.model.Jenkins
+import jenkins.plugins.git.GitSCMSource
+import org.jenkinsci.plugins.workflow.libs.*
+import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration
+import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever
+ 
+// parameters
+def globalLibrariesParameters = [
+  branch:               "master",
+  credentialId:         "git-credentials",
+  implicit:             false,
+  name:                 "jenkins-shared-lib",
+  repository:           "repo_url"
+]
+ 
+// define global library
+GitSCMSource gitSCMSource = new GitSCMSource(
+  "global-shared-library",
+  globalLibrariesParameters.repository,
+  globalLibrariesParameters.credentialId,
+  "*",
+  "",
+  false
+)
+ 
+// define retriever
+SCMSourceRetriever sCMSourceRetriever = new SCMSourceRetriever(gitSCMSource)
+ 
+// get Jenkins instance
+Jenkins jenkins = Jenkins.getInstance()
+ 
+// get Jenkins Global Libraries
+def globalLibraries = jenkins.getDescriptor("org.jenkinsci.plugins.workflow.libs.GlobalLibraries")
+ 
+// define new library configuration
+LibraryConfiguration libraryConfiguration = new LibraryConfiguration(globalLibrariesParameters.name, sCMSourceRetriever)
+libraryConfiguration.setDefaultVersion(globalLibrariesParameters.branch)
+libraryConfiguration.setImplicit(globalLibrariesParameters.implicit)
+ 
+// set new Jenkins Global Library
+globalLibraries.get().setLibraries([libraryConfiguration])
+ 
+// save current Jenkins state to disk
+jenkins.save()
