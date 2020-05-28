bf32f0cb2c897c98c930587e697ed61485aec07b
Subhash Kumar Samota <subhashsamota12@gmail.com>
2020-05-28 14:23:13 +0530

    Create github.groovy

---
+++ jenkins/groovy/scripts/github.groovy
@@ -0,0 +1,25 @@
+import org.jenkinsci.plugins.github.config.GitHubPluginConfig
+import org.jenkinsci.plugins.github.config.GitHubServerConfig
+
+def github = jenkins.model.Jenkins.instance.getExtensionList(GitHubPluginConfig.class)[0]
+
+
+def github_credentials_id = "GITHUB_SERVICE_TOKEN"
+def env = System.getenv()
+def created = false
+for (e in env) { 
+    if(e.key.contains(github_credentials_id) && e.value.trim() != null){
+        def config = new GitHubServerConfig(github_credentials_id)
+        config.setName("Tesco")
+        config.setApiUrl("<github_api_url>")
+        config.setManageHooks(false)
+
+        github.setConfigs([config])
+        github.save()
+        created = true;
+    }
+}
+
+if (!created){
+    println "Github plugin is not configured."
+}
